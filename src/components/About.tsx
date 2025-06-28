91b196cead7926441c8c92c0546c2b80e0116c2f
Siva Rama Krishna Reddy <153167302+kunchalas2023@users.noreply.github.com>
2025-06-28 17:03:23 -0400

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -118,12 +118,12 @@ const About = () => {
           <h2 className="text-4xl md:text-5xl font-bold text-gray-900 mb-6">About Me</h2>
           <div className="w-24 h-1 bg-gradient-to-r from-blue-500 to-purple-500 mx-auto mb-8"></div>
           <p className="text-xl text-gray-600 max-w-3xl mx-auto leading-relaxed">
-          I’m a dedicated cloud computing professional with a passion for building innovative solutions in the cloud. I hold a Master's degree in Computer Science from Florida Atlantic University, where I focused on Cloud Security, AI, and IoT.
+          I’m a dedicated cloud computing professional with a passion for building innovative solutions in the cloud. I hold a Master's degree in Computer Science from Florida Atlantic University, where I focused on Cloud Security, AI, and IoT. With more than 4 years of hands-on experience, I've developed expertise in AWS, Google Cloud, Terraform, and Python. I've worked on impactful projects such as designing cloud backup systems and creating AI-driven audio analysis tools. My career has been driven by a desire to optimize infrastructure and deliver scalable, secure cloud solutions.
 
-          With more than 4 years of hands-on experience, I've developed expertise in AWS, Google Cloud, Terraform, and Python. I've worked on impactful projects such as designing cloud backup systems and creating AI-driven audio analysis tools. My career has been driven by a desire to optimize infrastructure and deliver scalable, secure cloud solutions.
+          I am now transitioning into a Data Engineer role, looking to apply my cloud and data engineering skills to build scalable data pipelines, automate data workflows, and leverage big data technologies. I’m excited to contribute to a team that values innovation, scalability, and data-driven decision-making, while continuing to grow in a dynamic, forward-thinking environment.
             </p>
            <p className="text-xl text-gray-600 max-w-3xl mx-auto leading-relaxed">
-          I am currently looking for new opportunities where I can apply my cloud computing skills and continue to grow in a dynamic, forward-thinking environment. I’m excited to contribute to a team that values innovation, scalability, and problem-solving.
+          I am currently looking for new opportunities where I can apply my cloud computing and Data engineer skills and continue to grow in a dynamic, forward-thinking environment. I’m excited to contribute to a team that values innovation, scalability, and problem-solving.
           </p>
         </div>
 
